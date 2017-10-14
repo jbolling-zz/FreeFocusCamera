a986cd24d8c6e17acf83d4897595e5be4560c3a9
jbolling <josephcbolling@gmail.com>
2017-10-13 19:20:01 -0700

    adding arduino projects

---
+++ Arduino Code/change_device_id/change_device_id.ino
@@ -0,0 +1,70 @@
+#include <SoftwareSerial.h>
+
+// Test motor joint mode
+
+#include "DynamixelMotor.h"
+
+// id of the motor
+const uint8_t id=2;
+
+
+// communication baudrate
+const long unsigned int baudrate = 1000000;
+const long unsigned int serial_baudrate = 1200;
+
+// hardware serial without tristate buffer
+// see blink_led example, and adapt to your configuration
+#define SOFT_RX_PIN 3
+#define SOFT_TX_PIN 4
+
+SoftwareDynamixelInterface interface(SOFT_RX_PIN, SOFT_TX_PIN);
+//HardwareDynamixelInterface interface(Serial);
+//DynamixelMotor motor(interface, id);
+DynamixelDevice broadcast_device(interface, BROADCAST_ID);
+
+uint8_t led_state=true;
+
+void setup()
+{ 
+  Serial.begin(serial_baudrate);
+  interface.begin(baudrate);
+  delay(100);
+  
+  // check if we can communicate with the motor
+  // if not, we turn the led on and stop here
+  //uint8_t status=broadcast_device.init();
+  //if(status!=DYN_STATUS_OK)
+  //{
+  //  pinMode(LED_BUILTIN, OUTPUT);
+  //  digitalWrite(LED_BUILTIN, HIGH);
+  //  while(1);
+  //}
+  delay(1000);
+
+  uint8_t status = broadcast_device.changeId(2);
+  Serial.print(status);
+  delay(1000);
+  Serial.print(broadcast_device.id());
+  delay(1000);
+
+  if(status!=DYN_STATUS_OK)
+  {
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW);
+    while(1);
+  }
+}
+
+void loop() 
+{
+ 
+  broadcast_device.write(DYN_ADDRESS_LED, led_state);
+  led_state=!led_state;
+  Serial.print(broadcast_device.status());
+  delay(1000);
+  
+  //motor.led(HIGH);
+  //delay(1000);
+  //motor.led(LOW);
+  //delay(1000);
+}
---
+++ Arduino Code/rotate_and_capture_pixel/rotate_and_capture_pixel.ino
@@ -0,0 +1,103 @@
+#include <SoftwareSerial.h>
+#include "DynamixelMotor.h"
+
+// id of the motor
+DynamixelID pitch_id=2;
+DynamixelID yaw_id=1;
+// speed, between 0 and 1023
+int16_t speed=512;
+
+
+// communication baudrates
+const long unsigned int motor_baudrate = 1000000;
+const long unsigned int serial_baudrate = 1200;
+
+// hardware serial without tristate buffer
+// see blink_led example, and adapt to your configuration
+#define SOFT_RX_PIN 6
+#define SOFT_TX_PIN 5
+
+SoftwareDynamixelInterface interface(SOFT_RX_PIN, SOFT_TX_PIN);
+DynamixelMotor pitch_motor(interface, pitch_id);
+DynamixelMotor yaw_motor(interface, yaw_id);
+
+//DynamixelDevice broadcast_device(interface, BROADCAST_ID);
+
+String inString = "";
+
+
+enum readerState {y_pos, p_pos, finish};
+
+void output_status(uint8_t yaw_s, uint8_t pitch_s, String note){
+  Serial.println("[yaw, pitch] " + note + " status:");
+  Serial.println((String)"[" + yaw_s + ", " + pitch_s + "]");
+}
+
+void goto_positions(uint16_t yaw, uint16_t pitch){
+  yaw_motor.goalPosition(yaw);
+  delay(500);
+  pitch_motor.goalPosition(pitch);
+  delay(500);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(serial_baudrate);
+  interface.begin(motor_baudrate);
+  delay(100);
+  
+  // Initialize Motors
+  uint8_t yaw_status = yaw_motor.init();
+  uint8_t pitch_status=pitch_motor.init();
+  delay(100);
+  output_status(yaw_status,pitch_status, "init");
+  
+  yaw_motor.enableTorque();
+  pitch_motor.enableTorque();
+  delay(100);
+
+  yaw_motor.jointMode(204,820);
+  pitch_motor.jointMode(204,820);
+  delay(100);
+  
+  yaw_motor.speed(speed);
+  pitch_motor.speed(speed);
+  delay(100);
+}
+
+void loop() {
+  // Read serial input:
+  readerState state = y_pos;
+  uint16_t yaw_position = 512;
+  uint16_t pitch_position = 512;
+  while (Serial.available() > 0) {
+    int inChar = Serial.read();
+    switch (state){
+      case y_pos:
+        if (isDigit(inChar)) {
+          inString += (char)inChar;
+        }
+        else {
+          yaw_position = inString.toInt();
+          inString = "";
+          state = p_pos;
+        }
+        break;
+      case p_pos:
+        if (isDigit(inChar)) {
+          inString += (char)inChar;
+        }
+        else {
+          pitch_position = inString.toInt();
+          inString = "";
+          state = finish;
+        }
+        break;
+      case finish:
+        break;
+    }
+  }
+  goto_positions(yaw_position, pitch_position);
+  output_status(yaw_position, pitch_position, "position");
+}
+
