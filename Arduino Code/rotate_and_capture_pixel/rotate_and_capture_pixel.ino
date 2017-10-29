08350415139d0a08e6d5f9a486289bd1cbd12b82
jbolling <josephcbolling@gmail.com>
2017-10-28 18:08:21 -0700

    bugfixes and test updates to loop:

--- Arduino Code/rotate_and_capture_pixel/rotate_and_capture_pixel.ino
+++ Arduino Code/rotate_and_capture_pixel/rotate_and_capture_pixel.ino
@@ -7,10 +7,9 @@ DynamixelID yaw_id=1;
 // speed, between 0 and 1023
 int16_t speed=512;
 
-
 // communication baudrates
 const long unsigned int motor_baudrate = 1000000;
-const long unsigned int serial_baudrate = 1200;
+const long unsigned int serial_baudrate = 38400;
 
 // hardware serial without tristate buffer
 // see blink_led example, and adapt to your configuration
@@ -24,11 +23,17 @@ DynamixelMotor yaw_motor(interface, yaw_id);
 //DynamixelDevice broadcast_device(interface, BROADCAST_ID);
 
 String inString = "";
-
+uint16_t yaw_position;
+uint16_t pitch_position;
 
 enum readerState {y_pos, p_pos, finish};
 
 void output_status(uint8_t yaw_s, uint8_t pitch_s, String note){
+  Serial.println("[yaw, pitch] " + note + " status:");
+  Serial.println((String)"[" + yaw_s + ", " + pitch_s + "]");
+}
+
+void output_status(uint16_t yaw_s, uint16_t pitch_s, String note){
   Serial.println("[yaw, pitch] " + note + " status:");
   Serial.println((String)"[" + yaw_s + ", " + pitch_s + "]");
 }
@@ -56,20 +61,24 @@ void setup() {
   pitch_motor.enableTorque();
   delay(100);
 
-  yaw_motor.jointMode(204,820);
-  pitch_motor.jointMode(204,820);
+  yaw_motor.jointMode(300,724);
+  pitch_motor.jointMode(300,724);
   delay(100);
   
   yaw_motor.speed(speed);
   pitch_motor.speed(speed);
   delay(100);
+  
+  yaw_position = 512;
+  pitch_position = 512;
+
+  //initialize TCS Color Sensor
+  
 }
 
 void loop() {
   // Read serial input:
   readerState state = y_pos;
-  uint16_t yaw_position = 512;
-  uint16_t pitch_position = 512;
   while (Serial.available() > 0) {
     int inChar = Serial.read();
     switch (state){
@@ -98,6 +107,6 @@ void loop() {
     }
   }
   goto_positions(yaw_position, pitch_position);
-  output_status(yaw_position, pitch_position, "position");
+  //output_status(yaw_position, pitch_position, "position");
 }
 
