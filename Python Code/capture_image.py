ddb6a55673b43d455e6256fd7ccebff383359715
jbolling <josephcbolling@gmail.com>
2017-12-25 17:06:13 -0800

    updating script to print mages

--- Python Code/capture_image.py
+++ Python Code/capture_image.py
@@ -1,20 +1,30 @@
 import sys
 import numpy as np
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
 from image_templates import *
 from camera_wrapper import *
 
-#Get template files and sanitize
+# Read in angle data from template file
 if(len(sys.argv) == 1):
 	print('Syntax - capture_imge.py <template directory>')
 	quit()
-
 angles = load_angles(sys.argv[1])
-print angles[0,0,:]
+
+# Construct pixel array
 M = angles.shape[0]
 N = angles.shape[1]
 pixels = np.zeros((M,N,3))
 
+# Open camera connection
 print "Connecting to camera"
 with CameraWrapper("") as wrapper:
-	print "Requesting Pixel"
-	print wrapper.get_pixel([0,0])
+	# Read pixels one by one from camera
+	for i in range(0,M):
+		for j in range(0,N):
+			pixels[i,j,:] = wrapper.get_pixel(angles[i,j,:])
+			print 'Pixel returned from camera'
+			
+# Show image
+imgplot = plt.imshow(pixels)
+plt.show(block=True)
