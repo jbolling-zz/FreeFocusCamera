be1a2bceae2dfb506d26922836848282399d701a
jbolling <josephcbolling@gmail.com>
2018-04-18 22:23:39 -0700

    adding monitoring and extra image storage methods

--- Python Code/capture_image.py
+++ Python Code/capture_image.py
@@ -4,6 +4,8 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from image_templates import *
 from camera_wrapper import *
+#from scipy.misc import imsave
+import cv2 as cv
 
 # Read in angle data from template file
 if(len(sys.argv) == 1):
@@ -18,13 +20,17 @@ pixels = np.zeros((M,N,3))
 
 # Open camera connection
 print "Connecting to camera"
-with CameraWrapper("") as wrapper:
+with CameraWrapper('COM3') as wrapper:
 	# Read pixels one by one from camera
 	for i in range(0,M):
 		for j in range(0,N):
 			pixels[i,j,:] = wrapper.get_pixel(angles[i,j,:])
-			print 'Pixel returned from camera'
+			print 'Pixel returned from camera:'
+			print("(" + str(i) + "," + str(j) + "): " + str(pixels[i,j,:]))
 			
 # Show image
 imgplot = plt.imshow(pixels)
+cv.imwrite('output.png',pixels)
+np.save('output.npz',pixels)
+
 plt.show(block=True)
